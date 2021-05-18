5b05bb4a8dc44b58cbcbf644f666297f3158623d
huangyq <15797701413@163.com>
2021-05-18 21:14:20 +0800

    test mode code3

---
+++ redis/src/main/java/com/hyq/redis/TestPush.java
@@ -0,0 +1,11 @@
+package com.hyq.redis;
+
+/**
+ * @author huangyq
+ * @date 2021-05-18
+ */
+public class TestPush {
+    public static void main(String[] args) {
+        System.out.println("Test Test Test Test !!!");
+    }
+}
