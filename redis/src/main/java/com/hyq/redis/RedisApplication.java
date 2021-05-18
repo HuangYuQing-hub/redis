2777fad65b2439003cd52abf212f8927f8825fb1
huangyq <15797701413@163.com>
2021-05-18 21:01:32 +0800

    test mode code

--- redis/src/main/java/com/hyq/redis/RedisApplication.java
+++ redis/src/main/java/com/hyq/redis/RedisApplication.java
@@ -8,6 +8,10 @@ public class RedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
+
+
+        // test mod the code
+        System.out.println("Test the mod code  to push to orgin master!");
     }
 
 }
